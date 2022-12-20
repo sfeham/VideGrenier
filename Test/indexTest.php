e60c3c7a3e275413dc301a83935cfc1d17ec7c3a
Sofiane Hamlaoui <sofiane.hmbs@gmail.com>
2022-12-20 15:12:50 +0100

    Testunitaire

---
+++ Test/indexTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class indexTest extends TestCase
+{
+    /** @test */
+    public function testPushAndPop()
+    {
+        
+        $this->assertSame(0, 0);
+    }
+}
