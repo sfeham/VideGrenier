a574e0e3d8f46ed5da7d044a1e4a14deecc517ca
Yassin Zairi <yassinzairi0@gmail.com>
2022-12-20 13:41:38 +0100

    restore style and login link

--- App/Config.php
+++ App/Config.php
@@ -26,13 +26,13 @@ class Config
      * Database user
      * @var string
      */
-    const DB_USER = 'webapplication';
+    const DB_USER = 'root';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = '653rag9T';
+    const DB_PASSWORD = 'root';
 
     /**
      * Show or hide error messages on screen
